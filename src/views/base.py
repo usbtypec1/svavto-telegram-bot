8b221abef5a5ffb36f8a7eaad8f0b5622c283b3a
Eldos <eldos.baktybekov@gmail.com>
2024-10-01 11:03:56 +0600

    :lipstick: Add base TextView

---
+++ src/views/base.py
@@ -0,0 +1,35 @@
+from typing import TypeAlias
+
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ForceReply,
+    ReplyKeyboardRemove, Message,
+)
+
+__all__ = ('ReplyMarkup', 'TextView', 'answer_view')
+
+ReplyMarkup: TypeAlias = (
+        InlineKeyboardMarkup
+        | ReplyKeyboardMarkup
+        | ForceReply
+        | ReplyKeyboardRemove
+)
+
+
+class TextView:
+    text: str | None = None
+    reply_markup: ReplyMarkup | None = None
+
+    def get_text(self) -> str | None:
+        return self.text
+
+    def get_reply_markup(self) -> ReplyMarkup | None:
+        return self.reply_markup
+
+
+async def answer_view(message: Message, view: TextView) -> Message:
+    return await message.answer(
+        text=view.get_text(),
+        reply_markup=view.get_reply_markup(),
+    )
