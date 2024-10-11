f5baf9527df61b0cff39601e08299e733aaf8d2a
Eldos <eldos.baktybekov@gmail.com>
2024-10-11 11:04:59 +0600

    :sparkles: Add new api methods in performer repo

--- src/connections/performers.py
+++ src/connections/performers.py
@@ -13,16 +13,30 @@ class PerformerConnection(ApiConnection):
 
     async def create(
             self,
-            user_id: int,
+            telegram_id: int,
             full_name: str,
             car_sharing_phone_number: str,
             console_phone_number: str,
     ) -> httpx.Response:
         url = '/performers/'
         request_data = {
-            'telegram_id': user_id,
+            'telegram_id': telegram_id,
             'full_name': full_name,
             'car_sharing_phone_number': car_sharing_phone_number,
             'console_phone_number': console_phone_number,
         }
         return await self._http_client.post(url, json=request_data)
+
+    async def get_all(self) -> httpx.Response:
+        url = '/performers/'
+        return await self._http_client.get(url)
+
+    async def update_by_telegram_id(
+            self,
+            *,
+            telegram_id: int,
+            is_banned: bool,
+    ) -> httpx.Response:
+        url = f'/performers/{telegram_id}/'
+        request_data = {'is_banned': is_banned}
+        return await self._http_client.put(url, json=request_data)
--- src/repositories/performers.py
+++ src/repositories/performers.py
@@ -15,3 +15,32 @@ class PerformerRepository:
         response_data = response.json()
         handle_errors(response)
         return Performer.model_validate(response_data)
+
+    async def get_all(self) -> list[Performer]:
+        response = await self.__connection.get_all()
+        response_data = response.json()
+        handle_errors(response)
+        return [Performer.model_validate(item) for item in response_data]
+
+    async def create(self, performer: Performer) -> Performer:
+        response = await self.__connection.create(
+            telegram_id=performer.telegram_id,
+            full_name=performer.full_name,
+            car_sharing_phone_number=performer.car_sharing_phone_number,
+            console_phone_number=performer.console_phone_number,
+        )
+        response_data = response.json()
+        handle_errors(response)
+        return Performer.model_validate(response_data)
+
+    async def update_by_telegram_id(
+            self,
+            *,
+            telegram_id: int,
+            is_banned: bool,
+    ) -> None:
+        response = await self.__connection.update_by_telegram_id(
+            telegram_id=telegram_id,
+            is_banned=is_banned,
+        )
+        handle_errors(response)
