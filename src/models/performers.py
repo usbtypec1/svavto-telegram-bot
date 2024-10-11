a49bb6e9772c64da24198e278975d3dad1fd5ed6
Eldos <eldos.baktybekov@gmail.com>
2024-10-11 11:05:20 +0600

    :label: Add extra fields in `Performer` model

--- src/models/performers.py
+++ src/models/performers.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 __all__ = ('Performer',)
 
 
 class Performer(BaseModel):
+    id: int
     telegram_id: int
     full_name: str
     car_sharing_phone_number: str
     console_phone_number: str
+    is_banned: bool
+    created_at: datetime
