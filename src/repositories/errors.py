39be9227fe3ab6c6382c8b328e5064c18edaaae9
Eldos <eldos.baktybekov@gmail.com>
2024-11-02 15:29:05 +0600

    :construction: WIP shift finish flow

--- src/repositories/errors.py
+++ src/repositories/errors.py
@@ -26,7 +26,9 @@ code_to_exception_class: dict[ServerApiErrorCode, type[Exception]] = {
     ServerApiErrorCode.STAFF_HAS_NO_ACTIVE_SHIFT: StaffHasNoActiveShiftError,
     ServerApiErrorCode.CAR_WASH_NOT_FOUND: CarWashNotFoundError,
     ServerApiErrorCode.CAR_WASH_SAME_AS_CURRENT: CarWashSameAsCurrentError,
-    ServerApiErrorCode.SHIFT_BY_DATE_NOT_FOUND: ShiftByDateNotFoundError,
+    ServerApiErrorCode.SHIFT_FOR_SPECIFIC_DATE_NOT_FOUND: (
+        ShiftByDateNotFoundError
+    ),
     ServerApiErrorCode.SHIFT_NOT_CONFIRMED: ShiftNotConfirmedError,
     ServerApiErrorCode.STAFF_HAS_ACTIVE_SHIFT: StaffHasActiveShiftError,
     ServerApiErrorCode.SHIFT_ALREADY_FINISHED: ShiftAlreadyFinishedError,
