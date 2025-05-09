2c96f00a24705dbfdf40dc224d4e6b26af83de6e
Eldos <eldos.baktybekov@gmail.com>
2025-05-09 19:24:35 +0600

    :sparkles: Implement staff type management and update actions

--- src/handlers/staff/update/__init__.py
+++ src/handlers/staff/update/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Router
 
-from . import ban_or_unban, schedule
+from . import ban_or_unban, schedule, type
 
 __all__ = ('router',)
 
 router = Router(name=__name__)
 router.include_router(ban_or_unban.router)
 router.include_router(schedule.router)
+router.include_router(type.router)
